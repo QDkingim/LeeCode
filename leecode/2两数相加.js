5d5e03debb093048702f7289b7a6d017ae2bf1f8
renxiangsan <renxiangsan666@163.com>
2022-07-13 17:30:40 +0800

    Update 2两数相加.js

--- leecode/2两数相加.js
+++ leecode/2两数相加.js
@@ -11,21 +11,5 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    let l1Arr, l2Arr = []
-    let i, j = 0
-
-    while (l1.next !== null) {
-        l1 = l1.next
-        let i = 0
-        l1Arr[i] = l1.val
-        i++
-    }
-
-    while (l2.next !== null) {
-        l2 = l2.next
-        l2Arr[j] = l2.val
-        j++
-    }
-    console.log(l1Arr, l2Arr);
-
+    
 };
