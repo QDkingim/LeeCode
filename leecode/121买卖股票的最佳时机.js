25933dc534d708aea564bf00bc72ffa5aa9fda61
renxiangsan <renxiangsan666@163.com>
2022-07-15 10:36:25 +0800

    +

---
+++ leecode/121买卖股票的最佳时机.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+// 暴力算法（超时了）
+// var maxProfit = function (prices) {
+//     let profitMax = 0 // 利润
+//     for (let i = 0; i < prices.length; i++) {
+//         for (let j = i + 1; j < prices.length; j++) {
+//             if (prices[j] - prices[i] > 0) { // 后一天的价格比前一天的大
+//                 let profit = prices[j] - prices[i]
+//                 profitMax = profit > profitMax ? profit : profitMax
+//             }
+//         }
+//     }
+
+//     return profitMax
+// };
+
+
+// 一次遍历
+// var maxProfit = function (prices) {
+//     let profitMax = 0
+//     let minPrice = 10000
+//     for (let i = 0; i < prices.length; i++) {
+//         if (prices[i] < minPrice) {
+//             minPrice = prices[i]
+//         } else if (profitMax < prices[i] - minPrice) {
+//             profitMax = prices[i] - minPrice
+//         }
+//     }
+//     return profitMax
+// };
+
+// 动态规划
+var maxProfit = function (prices) {
+    let profitMax = 0
+
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
--- leecode/350两个数组的交集 II.js
+++ leecode/350两个数组的交集 II.js
@@ -3,65 +3,95 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+// var intersect = function (nums1, nums2) {
+//     /**
+//      * 思路一:遍历num1中的值,看看在num2中出现几次，如出现2次或2次以上，就遍历num1看看该值在num1中出现几次，取2者之中小的次数，写入结果数组中
+//      * 
+//      */
+
+//     let result = []; // 返回数组
+//     let sum = 0
+//     let x = 0
+//     for (let i = 0; i < nums1.length; i++) {
+//         let sum1 = 0,
+//             sum2 = 0; // 判断在num1和num2中出现的次数
+
+//         if (result.length !== 0) {
+//             let flag = 0
+//             for (let a = 0; a < result.length; a++) {
+//                 if (nums1[i] !== result[a]) {
+//                     flag++
+//                 }
+//             }
+//             if (flag === result.length) {
+//                 for (let j = 0; j < nums2.length; j++) {
+//                     if (nums1[i] === nums2[j]) {
+//                         sum2++
+//                     }
+//                 }
+//             }
+
+//         } else {
+//             for (let j = 0; j < nums2.length; j++) {
+//                 if (nums1[i] === nums2[j]) {
+//                     sum2++
+//                 }
+//             }
+//         }
+
+
+
+//         if (sum2 > 0) {
+//             nums1.forEach(item => {
+//                 if (item === nums1[i]) {
+//                     sum1++
+//                 }
+//             })
+//         }
+
+//         min = Math.min(sum1, sum2)
+
+//         sum += min
+
+//         for (x; x < sum; x++) {
+//             result.push(nums1[i])
+//         }
+
+//         x = sum
+//     }
+
+//     return result
+
+// };
+
 var intersect = function (nums1, nums2) {
     /**
-     * 思路一:遍历num1中的值,看看在num2中出现几次，如出现2次或2次以上，就遍历num1看看该值在num1中出现几次，取2者之中小的次数，写入结果数组中
-     * 
+     * 思路二：哈希表
      */
+    let result = []
+    let map = new Map()
 
-    let result = []; // 返回数组
-    let sum = 0
-    let x = 0
     for (let i = 0; i < nums1.length; i++) {
-        let sum1 = 0,
-            sum2 = 0; // 判断在num1和num2中出现的次数
-
-        if (result.length !== 0) {
-            let flag = 0
-            for (let a = 0; a < result.length; a++) {
-                if (nums1[i] !== result[a]) {
-                    flag++
-                }
-            }
-            if (flag === result.length) {
-                for (let j = 0; j < nums2.length; j++) {
-                    if (nums1[i] === nums2[j]) {
-                        sum2++
-                    }
-                }
-            }
-
+        if (map.has(nums1[i])) {
+            let num = map.get(nums1[i])
+            num++
+            map.set(nums1[i], num)
         } else {
-            for (let j = 0; j < nums2.length; j++) {
-                if (nums1[i] === nums2[j]) {
-                    sum2++
-                }
-            }
+            map.set(nums1[i], 1)
         }
+    }
 
-
-
-        if (sum2 > 0) {
-            nums1.forEach(item => {
-                if (item === nums1[i]) {
-                    sum1++
-                }
-            })
+    for (let i = 0; i < nums2.length; i++) {
+        if (map.has(nums2[i]) && map.get(nums2[i]) !== 0) {
+            result.push(nums2[i])
+            let num = map.get(nums2[i])
+            num--
+            map.set(nums2[i], num)
         }
-
-        min = Math.min(sum1, sum2)
-
-        sum += min
-
-        for (x; x < sum; x++) {
-            result.push(nums1[i])
-        }
-
-        x = sum
     }
 
     return result
 
-};
+}
 
-console.log(intersect([1, 2, 2, 1], [1, 2]));
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
