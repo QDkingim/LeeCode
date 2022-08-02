e94729806bae31ae8d98639f7cdd6676522faa1e
renxiangsan <renxiangsan666@163.com>
2022-08-02 10:43:51 +0800

    +

--- leecode/5最长回文子串.js
+++ leecode/5最长回文子串.js
@@ -3,5 +3,14 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
+    let len = s.length // 字符串的长度
+    let left = 0,
+        right = 0 // 中心点两边的左右指针
+    let maxLength = 0 // 回文子串的最大长度
 
+    for (let i = 0; i < len; i++) {
+        let subLeft = i,
+            subRight = i;
+
+    }
 };
