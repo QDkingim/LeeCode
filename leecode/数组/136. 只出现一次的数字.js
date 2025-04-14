92cba076e4444572f00f310a49503c7320bb640a
renxiangsan <renxiangsan666@163.com>
2025-04-14 14:11:06 +0800

    *136. 只出现一次的数字

--- leecode/数组/136. 只出现一次的数字.js
+++ leecode/数组/136. 只出现一次的数字.js
@@ -2,6 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
-    
+var singleNumber = function (nums) {
+  let result = 0;
+  for (let num of nums) {
+    result ^= num;
+  }
+  return result;
 };
